09f2b48bba8a8f9815df4bf3f562a00322f725bd
Alvaro Gabriel Goulart Rodrigues <alvaro.gr@aluno.ifsc.edu.br>
2024-04-19 00:55:19 -0300

    Classes estudo de caso 17/04

---
+++ Classes/Produtos.java
@@ -0,0 +1,26 @@
+package classes;
+
+public class Produtos {
+	private String Nproduto; //Nome do produto
+	private String PVD; // Preço de venda
+	private String PC; // Preço de custo
+	public String getNproduto() {
+		return Nproduto;
+	}
+	public void setNproduto(String nproduto) {
+		Nproduto = nproduto;
+	}
+	public String getPVD() {
+		return PVD;
+	}
+	public void setPVD(String pVD) {
+		PVD = pVD;
+	}
+	public String getPC() {
+		return PC;
+	}
+	public void setPC(String pC) {
+		PC = pC;
+	}
+
+}
