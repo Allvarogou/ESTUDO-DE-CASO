615caa22beee1d5a5da1b0cdfa966775f3316d4c
Alvaro Gabriel Goulart Rodrigues <alvaro.gr@aluno.ifsc.edu.br>
2024-04-24 14:42:18 -0300

    Estrutura do projeto

--- SwitchCase/src/swit/Estoque.java
+++ SwitchCase/src/swit/Estoque.java
@@ -1,31 +1,39 @@
 package classes;
 
 public class Estoque {
-	private String Cat; // Categoria 
-	private String ID;  // ID
+	private String Cat; // Categoria
+	private String ID; // ID
 	private String CodBarra; // Codigo de barra
 	private Integer Estoque; // Quantidade em estoque
+
 	public String getCat() {
 		return Cat;
 	}
+
 	public void setCat(String cat) {
 		Cat = cat;
 	}
+
 	public String getID() {
 		return ID;
 	}
+
 	public void setID(String iD) {
 		ID = iD;
 	}
+
 	public String getCodBarra() {
 		return CodBarra;
 	}
+
 	public void setCodBarra(String codBarra) {
 		CodBarra = codBarra;
 	}
+
 	public Integer getEstoque() {
 		return Estoque;
 	}
+
 	public void setEstoque(Integer estoque) {
 		Estoque = estoque;
 	}
--- SwitchCase/src/swit/Produtos.java
+++ SwitchCase/src/swit/Produtos.java
@@ -1,24 +1,32 @@
 package classes;
 
 public class Produtos {
-	private String Nproduto; //Nome do produto
+
+	private String Nproduto; // Nome do produto
 	private String PVD; // Preço de venda
 	private String PC; // Preço de custo
+	private Fornecedor fornecedor;
+
 	public String getNproduto() {
 		return Nproduto;
 	}
+
 	public void setNproduto(String nproduto) {
 		Nproduto = nproduto;
 	}
+
 	public String getPVD() {
 		return PVD;
 	}
+
 	public void setPVD(String pVD) {
 		PVD = pVD;
 	}
+
 	public String getPC() {
 		return PC;
 	}
+
 	public void setPC(String pC) {
 		PC = pC;
 	}
