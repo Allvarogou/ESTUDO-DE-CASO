bb8ee261b5e24f4313b26abdad65c83b7a76ecc9
Alvaro Gabriel Goulart Rodrigues <alvaro.gr@aluno.ifsc.edu.br>
2024-05-08 15:14:56 -0300

    conteudo novo controle/modelo/visao

---
+++ SwitchCase/src/modelo/Fornecedor.java
@@ -0,0 +1,49 @@
+package modelo;
+
+import java.util.ArrayList;
+
+public class Fornecedor {
+
+	private String nome;
+	private int id;
+	private String marca; 
+	
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	public static void main(String[] args) {
+		
+		ArrayList<Fornecedor> forn = new ArrayList<>();
+		
+		Fornecedor f1 = new Fornecedor();
+		f1.setNome("F1 LTA");
+		f1.setId(001);
+		f1.setMarca("OUS");
+		
+		forn.add(f1);
+		
+		
+		
+	}
+
+}
---
+++ SwitchCase/src/modelo/Produto.java
@@ -0,0 +1,37 @@
+package modelo;
+
+public class Produto extends Fornecedor{ 
+ 
+	private String Nproduto; // Nome do produto 
+	private Float PVD; // Preço de venda 
+	private Float PC; // Preço de custo 
+	private Fornecedor fornecedor; 
+	 
+	public String getNproduto() { 
+		return Nproduto; 
+	} 
+	public void setNproduto(String nproduto) { 
+		Nproduto = nproduto; 
+	} 
+	public Float getPVD() { 
+		return PVD; 
+	} 
+	public void setPVD(Float pVD) { 
+		PVD = pVD; 
+	} 
+	public Float getPC() { 
+		return PC; 
+	} 
+	public void setPC(Float pC) { 
+		PC = pC; 
+	} 
+	public Fornecedor getFornecedor() { 
+		return fornecedor; 
+	} 
+	public void setFornecedor(Fornecedor fornecedor) { 
+		this.fornecedor = fornecedor; 
+	} 
+	 
+ 
+ 
+}
